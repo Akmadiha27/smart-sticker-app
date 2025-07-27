91d4152a3ea8582620c1f3b91d87302f268d489a
deb <43925933+lil-bunny@users.noreply.github.com>
2025-07-27 15:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from mcp.types import INTERNAL_ERROR, INVALID_PARAMS, TextContent ,ImageContent
 from  google import genai
 from google.genai import types 
 from  google.genai.types import Content,Part
-from app import add_event_final
+from googlecalendar_service import add_event_final
 
 MY_NUMBER = "YOUR NUMBER"# Insert your number {91}{Your number}
 mcp = FastMCP(
@@ -28,6 +28,46 @@ class RichToolDescription(BaseModel):
 
 
 #ADD YOUR CALENDAR TOOL HERE
+
+
+class CalendarToolDescription(BaseModel):
+    description: str
+    use_when: str
+    side_effects: str | None
+
+CalendarTool=CalendarToolDescription(
+    description="This is used to add events to google calendar",
+    use_when="whenever user /add-event and send event details then use this to add event to google calendar",
+    side_effects=None
+)
+class DateTimeWithTimeZone(BaseModel):
+    dateTime: str = Field(description="Date and time in YYYY-MM-DDTHH:MM:SS+HH:MM format")
+    timeZone: str = Field(description="Time zone, e.g., Asia/Kolkata")
+
+class CalendarEvent(BaseModel):
+    summary: str = Field(description="Name or summary of the event")
+    location: str = Field(description="Location of the event")
+    description: str = Field(description="Description of the event")
+    start: DateTimeWithTimeZone = Field(description="Start date, time, and time zone")
+    end: DateTimeWithTimeZone = Field(description="End date, time, and time zone")
+
+@mcp.tool(description=CalendarTool.model_dump_json())
+async def add_event(event: CalendarEvent):
+    event_dict = {
+            "summary": event.summary,
+            "location": event.location,
+            "description": event.description,
+            "start": {
+                "dateTime": event.start.dateTime,
+                "timeZone": event.start.timeZone
+            },
+            "end": {
+                "dateTime": event.end.dateTime,
+                "timeZone": event.end.timeZone
+            }}
+    print(event_dict)
+    result=await add_event_final(event_dict)
+    return result
 
 @mcp.tool()
 async def validate() -> str:
