3625f7a16b4e2b0e8f3c0fa55c248bcb31e08639
Asadgit_1811 <asadmohammed181105@gmail.com>
2025-08-10 16:07:46 +0530

    Make build script executable and ready for Render

--- render-build.sh
+++ render-build.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
-# Install system dependencies for Pillow & rembg
-apt-get update && apt-get install -y \
-    libgl1 \
-    libglib2.0-0 \
-    libjpeg-dev \
-    zlib1g-dev \
-    libpng-dev \
-    libtiff-dev \
-    libopenjp2-7 \
-    libwebp-dev \
-    libavif-dev \
-    libturbojpeg-dev \
-    gcc \
-    g++ \
-    python3-dev
+# Exit on error
+set -o errexit
 
-# Upgrade pip & install Python packages
-pip install --upgrade pip
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
